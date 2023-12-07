1eae7627a7ce91d9f764fac0295d0793b1b4c623
adammaher1 <B00148489@mytudublin.ie>
2023-12-07 17:21:57 +0000

    Ass2

--- src/app/authCheck/route.js
+++ src/app/authCheck/route.js
@@ -1,11 +1,11 @@
-import { cookies } from 'next/headers'
+import { cookies } from 'next/headers';
 export async function GET(req, res) {
 // Make a note we are on
 // the api. This goes to the console.
-console.log("checking auth")
+//console.log("checking auth")
 // get the auth record
-let record = cookies().get('auth');
-console.log(record.value);
+//let record = cookies().get('auth');
+//console.log(record.value);
 // at the end of the process we need to send something back.
-return Response.json({ "status":"" + record.value + ""})
+return Response.json({ "status":"" + "" + ""})
 }
