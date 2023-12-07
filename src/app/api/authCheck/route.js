1fc6a5a9b203614e96c865f2e74af985a9de00e2
adammaher1 <B00148489@mytudublin.ie>
2023-12-07 17:35:03 +0000

    Ass2

--- src/app/api/authCheck/route.js
+++ src/app/api/authCheck/route.js
@@ -1,4 +1,4 @@
-import { cookies } from 'next/headers'
+import { cookies } from 'next/headers';
 export async function GET(req, res) {
 // Make a note we are on
 // the api. This goes to the console.
