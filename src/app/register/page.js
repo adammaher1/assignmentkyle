2350c0ddfae431237bab5ebeab3100294db7662b
adammaher1 <B00148489@mytudublin.ie>
2023-12-07 15:29:11 +0000

    Ass2

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -15,147 +15,190 @@ import Container from '@mui/material/Container';
 import {ThemeProvider } from '@mui/material/styles';
 
 import { createTheme } from '@mui/material/styles';
-import { green, purple, teal } from '@mui/material/colors';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
+import { green, purple } from '@mui/material/colors';
 
-
-
-// Ensure you have the necessary imports here
 
 export default function Page() {
+
+
+  /*
+  This function does the actual work
+  calling the fetch to get things from the database.
+  */ 
+  async function runDBCallAsync(url) {
+
+
+    const res = await fetch(url);
+    const data = await res.json();
+
+ 
+    if(data.data== "true"){
+      console.log("registered");
+
+    } else {
+
+      console.log("not valid (register page)");
+    }
+  }
+
+
+  /*
+
+  When the button is clicked, this is the event that is fired.
+  The first thing we need to do is prevent the default refresh of the page.
+  */
+	const handleSubmit = (event) => {
+		
+		console.log("handling submit");
+
+
+    event.preventDefault();
+  
+		const data = new FormData(event.currentTarget);
+
+        let fname = data.get('fname');
+        let lname = data.get('lname');
+        let email = data.get('email');
+		let pass = data.get('pass');
+        let dob = data.get('dob');
+
+        console.log("Sent fname:" + fname);
+        console.log("Sent lname:" + lname);
+        console.log("Sent email:" + email);
+        console.log("Sent pass:" + pass);
+        console.log("Sent dob:" + dob);
+
+
+
+    runDBCallAsync(`api/login?email=${email}&pass=${pass}`);
+
+  }; // end handler
+
+
   const theme = createTheme({
     palette: {
+     
       secondary: {
         main: green[500],
       },
     },
   });
 
-  async function runDBCallAsync(url) {
-    const res = await fetch(url);
-    const data = await res.json();
-
-    if (data.data === "valid") {
-      console.log("login is valid!");
-    } else {
-      console.log("not valid  ");
-    }
-  }
-
-  const validateForm = (event) => {
-    let errorMessage = '';
-    const data = new FormData(event.currentTarget);
-    let email = data.get('email');
-    var validator = require("email-validator");
-    let emailCheck = validator.validate(email);
-    console.log("email status" + emailCheck);
-    if (emailCheck === false) {
-      errorMessage += 'Incorrect email';
-    }
-    return errorMessage;
-  }
-
-  const handleSubmit = (event) => {
-    console.log("handling submit");
-    event.preventDefault();
-    let errorMessage = validateForm(event);
-    setErrorHolder(errorMessage);
-    if (errorMessage.length > 0) {
-      setOpen(true);
-    } else {
-      const data = new FormData(event.currentTarget);
-      let email = data.get('email');
-      let pass = data.get('pass');
-      console.log("Sent email:" + email);
-      console.log("Sent pass:" + pass);
-      console.log("calling db");
-      runDBCallAsync(`api/login?email=${email}&pass=${pass}`);
-    }
-  };
-
-  const [open, setOpen] = React.useState(false);
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  const [errorHolder, setErrorHolder] = React.useState('');
 
   return (
     <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}></Avatar>
-          <Typography component="h1" variant="h5">
-            Register
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
+    <Container component="main"  maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+        <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="fname"
+            label="First Name"
+            name="fname"
+            autoComplete="fname"
+            autoFocus
+          />
+           <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="lname"
+            label="Last Name"
+            name="lname"
+            autoComplete="lname"
+            autoFocus
+          />
+          <TextField
               margin="normal"
               required
               fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            {/* Add other form fields as needed */}
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Register
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-      </Container>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">{"Error"}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            {errorHolder}
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} autoFocus>
-            Close
+              name="dob"
+              label="dob"
+              type="text"
+              id="dob"
+              autoComplete=""
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="pass"
+            label="Password"
+            type="password"
+            id="pass"
+            autoComplete="current-password"
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="cpass"
+            label="Confirm Password"
+            type="password"
+            id="cpass"
+            autoComplete="confirm-password"
+          />  
+
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Register Now
           </Button>
-        </DialogActions>
-      </Dialog>
+
+
+
+
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="../register" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+
+    </Container>
+
     </ThemeProvider>
+
   );
 }
