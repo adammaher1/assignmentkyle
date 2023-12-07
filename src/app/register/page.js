e957e3b37465fa48474a8b9138ac65bf5d6838f6
adammaher1 <B00148489@mytudublin.ie>
2023-12-07 16:13:42 +0000

    Ass2

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -17,7 +17,11 @@ import {ThemeProvider } from '@mui/material/styles';
 import { createTheme } from '@mui/material/styles';
 import { green, purple } from '@mui/material/colors';
 
-
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 export default function Page() {
 
 
@@ -41,9 +45,55 @@ export default function Page() {
     }
   }
 
+  const validateForm = (event) => {
+    let errorMessage = ""
+
+    const data = new FormData(event.currentTarget);
+
+    //get email
+    let email = data.get('email');
+
+
+
+    //pull validator
+    var validator = require("email-validator");
+
+
+    //run validator
+    let emailCheck = validator.validate(email);
+
+
+    //prints status
+    console.log("email status" + emailCheck);
+
+
+    //if false, add error string
+    if (emailCheck == false){
+      errorMessage += 'Invalid Email';
+    }
+
+    // Validate the password
+    let pass = data.get('pass')
+    if(pass.length ==0){
+      errorMessage += '\nNo password added';
+    } else if(pass.length < 7){
+      errorMessage += '\nPassword is too short - Minimum 8 Characters';
+    }
+
+    let fname = data.get('fname')
+    if(fname.length == 0){
+      errorMessage += '\nNo first name added';
+    }
+
+    let lname = data.get('lname')
+    if(lname.length == 0){
+      errorMessage += '\nNo last name added';
+    }
+    return errorMessage;
+  }
+
 
   /*
-
   When the button is clicked, this is the event that is fired.
   The first thing we need to do is prevent the default refresh of the page.
   */
@@ -51,15 +101,26 @@ export default function Page() {
 		
 		console.log("handling submit");
 
+        event.preventDefault();
 
-    event.preventDefault();
-  
-		const data = new FormData(event.currentTarget);
+      //call validator
+      let errorMessage = validateForm(event);
+
+      // save the message
+      setErrorHolder(errorMessage);
+
+      //if we have error
+      if(errorMessage.length > 0){
+
+        setOpen(true);
+
+      }else {
+        const data = new FormData(event.currentTarget);
 
         let fname = data.get('fname');
         let lname = data.get('lname');
         let email = data.get('email');
-		let pass = data.get('pass');
+        let pass = data.get('pass');
         let dob = data.get('dob');
 
         console.log("Sent fname:" + fname);
@@ -69,9 +130,8 @@ export default function Page() {
         console.log("Sent dob:" + dob);
 
 
-
-    runDBCallAsync(`api/login?email=${email}&pass=${pass}`);
-
+        runDBCallAsync(`api/login?email=${email}&pass=${pass}`);
+      }
   }; // end handler
 
 
@@ -84,9 +144,43 @@ export default function Page() {
     },
   });
 
+  //first
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  //second
+  const [errorHolder, setErrorHolder] = React.useState(false);
 
   return (
     <ThemeProvider theme={theme}>
+      <React.Fragment>
+        <Dialog
+            open={open}
+            onClose = {handleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">
+            {"Error"}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              {errorHolder}
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} autoFocus>
+              Close
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </React.Fragment>
     <Container component="main"  maxWidth="xs">
       <CssBaseline />
       <Box
