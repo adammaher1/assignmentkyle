fa22b69f3856ac34c7644c9ee29039977ab7c579
adammaher1 <B00148489@mytudublin.ie>
2023-12-14 11:19:03 +0000

    Ass2

--- src/app/api/authCheck/route.js
+++ src/app/api/authCheck/route.js
@@ -1,4 +1,4 @@
-/*import { cookies } from 'next/headers'
+import { cookies } from 'next/headers'
 export async function GET(req, res) {
 // Make a note we are on
 // the api. This goes to the console.
@@ -9,4 +9,3 @@ console.log(record.value);
     // at the end of the process we need to send something back.
 return Response.json({ "status":"" + record.value + ""})
 }
-*/
